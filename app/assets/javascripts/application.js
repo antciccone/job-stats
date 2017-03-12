971e1bc9d65bfd00f6cd0ea62038432bdba3eba7
antciccone <aciccone@bu.edu>
2017-03-12 13:04:59 -0400

    add button and input field on dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  $('li').on('click', function(){
+    addInput(this)
+  })
+})
+
+function addInput(value) {
+  var text = value.innerText
+  $('.dropdown-button').after('<button style="margin-left: 10px;" class="waves-effect waves-light btn">Add to table</button><br>')
+  $('.waves-effect').after('<input  placeholder=' +  text + '="first_name" type="text" class="validate">')
+}
